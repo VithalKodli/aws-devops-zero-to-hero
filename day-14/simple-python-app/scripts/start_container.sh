87b34b7ac5fc50c07f4780fc3f53048282fb656e
Vithal kodli <vittalraokodli172@gmail.com>
2023-10-06 11:55:45 +0530

    new commit

--- day-14/simple-python-app/scripts/start_container.sh
+++ day-14/simple-python-app/scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push vithal99/demo-app
+docker push vithal99/simple-pytho-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vithal99/demo-app
+docker run -d -p 5000:5000 vithal99/simple-pytho-app
