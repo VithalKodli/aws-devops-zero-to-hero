b79bd1d92b736e4a83185dd22dd16b451833a163
Vithal kodli <vittalraokodli172@gmail.com>
2023-10-06 12:00:05 +0530

    hii

--- day-14/simple-python-app/scripts/start_container.sh
+++ day-14/simple-python-app/scripts/start_container.sh
@@ -2,7 +2,12 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push vithal99/simple-pytho-app
+docker pull vithal99/simple-pytho-app
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 vithal99/simple-pytho-app
+# Check if the image was pulled successfully
+if [ $? -eq 0 ]; then
+    # Run the Docker image as a container
+    docker run -d -p 5000:5000 vithal99/simple-pytho-app
+else
+    echo "Failed to pull the Docker image."
+fi
